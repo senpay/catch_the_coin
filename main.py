7a5532ac40d83572ec64e46aa6704c582176abfa
Alexander Pushkarev <alexspush@gmail.com>
2024-12-14 18:21:13 +0000

    final version without refactoring

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from model.world import World
 
 WORLD_WIDTH_PIXELS = World.WIDTH * World.TILE_SIZE
 WORLD_HEIGHT_PIXELS = World.HEIGHT * World.TILE_SIZE
+GAME_LENGTH_SECONDS = 30
 
 
 class App:
@@ -28,6 +29,7 @@ class App:
             y=generate_random_coordinate(WORLD_HEIGHT_PIXELS),
         )
         self.__score = 0
+        self.__framecount = 0
 
         pyxel.run(update=self.update, draw=self.draw)
 
@@ -35,6 +37,9 @@ class App:
         return sprites_collide(
             self.__player.x, self.__player.y, self.__coin.x, self.__coin.y
         )
+
+    def _time_run_out(self):
+        return self.__framecount > GAME_LENGTH_SECONDS * World.FPS
 
     def update(self):
         if self._has_catched_coin():
@@ -63,10 +68,33 @@ class App:
             self.__player.stop_moving()
 
     def draw(self):
+        if self._time_run_out():
+            pyxel.cls(0)
+            pyxel.text(
+                WORLD_WIDTH_PIXELS // 2 - 20,
+                WORLD_HEIGHT_PIXELS // 2,
+                "Game Over",
+                pyxel.COLOR_WHITE,
+            )
+            pyxel.text(
+                WORLD_WIDTH_PIXELS // 2 - 20,
+                WORLD_HEIGHT_PIXELS // 2 + 10,
+                f"Score: {self.__score}",
+                pyxel.COLOR_WHITE,
+            )
+            return
+
+        self.__framecount += 1
         pyxel.cls(0)
         self.__player.draw()
         self.__coin.draw()
         pyxel.text(5, 5, f"Score: {self.__score}", pyxel.COLOR_WHITE)
+        pyxel.text(
+            5,
+            15,
+            f"Time: {GAME_LENGTH_SECONDS - self.__framecount // World.FPS}",
+            pyxel.COLOR_WHITE,
+        )
 
 
 App()
