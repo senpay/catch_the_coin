6ca1cd8959f4e812917f4c489aaa1e0280598d0f
Alexander Pushkarev <alexspush@gmail.com>
2024-12-14 17:22:42 +0000

    Show wolrd with player

--- main.py
+++ main.py
@@ -1,51 +1,36 @@
 import pyxel
 
 from model.player import Player
-from model.world import World, WorldItem, world_item_draw, TILE_SIZE
+from model.world import World
 
 
 class App:
     def __init__(self):
-        pyxel.init( World.WIDTH * TILE_SIZE, World.HEIGHT * TILE_SIZE, title="Hello World")
+        pyxel.init( World.WIDTH * World.TILE_SIZE, World.HEIGHT * World.TILE_SIZE, title="Catch the coin")
         pyxel.load("mygame.pyxres")
 
-        self.world = World(pyxel.tilemap(0))
+        self.__player = Player(x=0, y=0)
 
-        self.player = Player(self.world)
-
-        pyxel.run(self.update, self.draw)
+        pyxel.run(update=self.update, draw=self.draw)
 
     def update(self):
         if pyxel.btn(pyxel.KEY_LEFT):
-            self.player.move_left()
+            self.__player.move_left()
         elif pyxel.btn(pyxel.KEY_RIGHT):
-            self.player.move_right()    
+            self.__player.move_right()    
         elif pyxel.btn(pyxel.KEY_UP):
-            self.player.move_up()
+            self.__player.move_up()
         elif pyxel.btn(pyxel.KEY_DOWN):
-            self.player.move_down()        
+            self.__player.move_down()        
         elif pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
         else:
-            self.player.stop_moving()
+            self.__player.stop_moving()
 
     def draw(self):
         pyxel.cls(0)
+        self.__player.draw()
 
-        for y in range(self.world.HEIGHT):
-            for x in range(self.world.WIDTH):
-                world_item = self.world.world_map[y][x]
-                world_item_draw(pyxel, x, y, world_item)
-
-        pyxel.blt(
-            self.player.x,
-            self.player.y,
-            self.player.IMG,
-            WorldItem.PLAYER[0] * TILE_SIZE,
-            WorldItem.PLAYER[1] * TILE_SIZE,
-            self.player.WIDTH,
-            self.player.HEIGHT,
-        )
 
 
 App()
--- model/player.py
+++ model/player.py
@@ -1,6 +1,7 @@
-from model.pyxel_utlis import PyxelSounds
+import pyxel
+
+from model.world import World
 from model.utils import round_float_if_close
-from model.world import TILE_SIZE, WorldItem, sprites_collide
 
 
 class Player:
@@ -9,12 +10,12 @@ class Player:
     HEIGHT = 8
     DX = 0.5
     ROUNDING_ERROR_DELTA = 0.1
+    PLAYER_IMG_X = 3
+    PLAYER_IMG_Y = 0
 
-    def __init__(self, world):
-        self.__x = world.player_grid_x * TILE_SIZE
-        self.__y = world.player_grid_y * TILE_SIZE
-        self.world = world
-        self.pyxel_sounds = PyxelSounds()
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
 
     def stop_moving(self):
         self.__x = round_float_if_close(self.__x, self.ROUNDING_ERROR_DELTA)
@@ -22,34 +23,7 @@ class Player:
 
     # dx should be 1 or -1 (for right or left)
     def move_horizontally(self, dx):
-        tile_y = int(self.__y / TILE_SIZE)
-        tile_x = int(self.__x / TILE_SIZE)
-
         new_x = self.__x + dx * self.DX
-
-        new_tile_x = tile_x + dx
-
-        if new_tile_x < 0 or new_tile_x >= self.world.WIDTH:
-            return
-
-        # We always round "Down", so we can consider possible "up" and "bottom" tiles
-        next_tile_up = self.world.world_map[tile_y][new_tile_x]
-        next_tile_bottom = self.world.world_map[tile_y + 1][new_tile_x]
-
-        if (
-            next_tile_up == WorldItem.WALL
-            and sprites_collide(
-                new_x, self.__y, new_tile_x * TILE_SIZE, tile_y * TILE_SIZE
-            )
-        ) or (
-            next_tile_bottom == WorldItem.WALL
-            and sprites_collide(
-                new_x, self.__y, new_tile_x * TILE_SIZE, (tile_y + 1) * TILE_SIZE
-            )
-        ):
-            self.pyxel_sounds.play_hit_wall_sound()
-            return
-
         self.__x = new_x       
 
     def move_left(self):
@@ -60,29 +34,7 @@ class Player:
 
     # dy should be 1 or -1 (for down or up)
     def move_vertically(self, dy):
-        tile_y = int(self.__y / TILE_SIZE)
-        tile_x = int(self.__x / TILE_SIZE)
-
         new_y = self.__y + dy * self.DX
-        new_tile_y = tile_y + dy
-
-        # We always round "Down", so we can consider possible "left" and "right" tiles
-        next_tile_left = self.world.world_map[new_tile_y][tile_x]
-        next_tile_right = self.world.world_map[new_tile_y][tile_x + 1]
-
-        if (
-            next_tile_left == WorldItem.WALL
-            and sprites_collide(
-                self.__x, new_y, tile_x * TILE_SIZE, new_tile_y * TILE_SIZE
-            )
-        ) or (
-            next_tile_right == WorldItem.WALL
-            and sprites_collide(
-                self.__x, new_y, (tile_x + 1) * TILE_SIZE, new_tile_y * TILE_SIZE
-            )
-        ):
-            self.pyxel_sounds.play_hit_wall_sound()
-            return
 
         self.__y = new_y
 
@@ -91,11 +43,14 @@ class Player:
 
     def move_down(self):
         self.move_vertically(1)
-
-    @property
-    def x(self):
-        return int(self.__x)
     
-    @property
-    def y(self):
-        return int(self.__y)
+    def draw(self):  
+        pyxel.blt(
+            self.__x,
+            self.__y,
+            self.IMG,
+            self.PLAYER_IMG_X * World.TILE_SIZE,
+            self.PLAYER_IMG_Y * World.TILE_SIZE,
+            self.WIDTH,
+            self.HEIGHT,
+        )
